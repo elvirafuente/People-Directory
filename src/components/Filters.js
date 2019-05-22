361ff50ccd0a19adaf0507e369cf640ed11bdf1f
Elvira Fuente <hebebicho@gmail.com>
2019-05-22 12:50:34 +0200

    add checkboxes

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 class Filters extends React.Component{
     render(){
         return (
-            <p>Filters</p>
+            <Fragment>
+                <h2>Filters</h2>
+                <form>
+                    <fieldset>
+                        <legend>Filter by gender</legend>
+                        <label>
+                            <input type="checkbox" value="female" name="genders" checked={true}/>
+                            Female
+                        </label>
+                        <label>
+                            <input type="checkbox" value="male" name="genders" checked={true}/>
+                            Male
+                        </label>
+                    </fieldset>
+                </form>
+            </Fragment>
         )
     }
 
--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -15,7 +15,6 @@ class PeopleList extends React.Component {
             )}
             </ul>
             </Fragment>
-            
         )
     }
 
